ed19955d840f6183f6fb8731f9bd5f31fc005adb
beefarino <jimchristopher@gmail.com>
2014-04-23 16:03:25 -0400

    fix build script

--- default.ps1
+++ default.ps1
@@ -1,5 +1,5 @@
 ﻿#
-# Copyright (c) 2012 Code Owls LLC
+# Copyright (c) 2014 Code Owls LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy 
 # of this software and associated documentation files (the "Software"), to 
@@ -19,7 +19,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 # IN THE SOFTWARE. 
 # 
-# 	psake build script for the SQLite provider and module
+# 	psake build script for the P2F library
 #
 # 	valid configurations:
 #  		Debug
@@ -33,15 +33,14 @@ properties {
 	$local = './_local';
 	$keyContainer = '';
 	$slnFile = @(
-		'./src/SQLiteProvider.sln'
+		'./src/ProviderFramework.sln'
 	);	
 	$libPath = "./lib"
-    $targetPath = "./src/CodeOwls.PowerShell.SQLite.Provider/bin";
-	$moduleSource = "./src/Modules";
-    $metadataAssembly = 'CodeOwls.PowerShell.SQLite.Provider.dll'
+    $targetPath = "./src/P2F/CodeOwls.PowerShell.Provider/bin";
+    $metadataAssembly = 'CodeOwls.PowerShell.Provider.dll'
 };
 
-$framework = '4.0'
+framework '4.0'
 
 function get-packageDirectory
 {
@@ -76,92 +75,65 @@ function create-PackageDirectory( [Parameter(ValueFromPipeline=$true)]$packageDi
     }    
 }
 
-function get-ProviderVersion
-{
-	$p = get-modulePackageDirectory;    
-    $md = join-path $p "SQLite\bin\$metadataAssembly";
-	Write-Verbose "reading metadata from assembly at path $md";
-    ( get-item $md | select -exp versioninfo | select -exp productversion );
+task default -depends Build;
+
+$private = "this is a private task not meant for external use";
+
+# private tasks 
+task __VerifyConfiguration -description $private {
+	Assert ( @('Debug', 'Release') -contains $config ) "Unknown configuration, $config; expecting 'Debug' or 'Release'";
+	Assert ( Test-Path $slnFile ) "Cannot find solution, $slnFile";
+	
+	Write-Verbose ("packageDirectory: " + ( get-packageDirectory ));
 }
 
-task default -depends Install;
-
-. ./psake/commontask.ps1
-
-# test tasks
-
-task Test -depends Build,Package,Install,__CreateLocalDataDirectory -description "executes functional tests" {
-	Write-Verbose "running tests ...";
-	
-	# we run tests in a nested powershell session so binary modules will be unloaded from memory
-	# 	after the tests are complete.
-	#	this will allow us to rerun the build from the same console.
-	$xmlfile =  './_local/localtests.xml';
-	
-	powershell -outputformat XML -command "cd ./tests | out-null; ./invoke-tests.ps1 -erroraction 'silentlycontinue' -warningaction 'silentlycontinue'  " | Export-Clixml $xmlfile -Force;
-	$results = Import-Clixml $xmlfile;
-	
-	#drop the results to the console output
-	$results;	
-	
-	$failures = $results | where { -not $_.result };
-	if( $failures )
-	{	
-		throw "one or more tests have failed"
-	}
+task __CreatePackageDirectory -description $private {
+	get-packageDirectory | create-packageDirectory;		
 }
 
-# package tasks
-
-task PackageModule -depends CleanModule,Build,__CreateModulePackageDirectory -description "assembles module distribution file hive" -action {
-	$mp = get-modulePackageDirectory;
-	
-	# copy module src hive to distribution hive
-	Copy-Item $moduleSource -container -recurse -Destination $mp -Force;
-	
-	# copy bins to module bin area
-	mkdir "$mp\SQLite\bin" | Out-Null;
-	ls "$targetPath/$config" | copy-item -dest "$mp\SQLite\bin" -recurse -force;
-	
-	# copy sqlite dlls to module bin area
-	# 	note there are two flavors: x32 and x64; we keep them in unique subfolders and only
-	#	load the one we need at runtime
-	ls $libPath -Filter x* | Copy-Item -Destination "$mp\SQLite\bin" -Container -Recurse -Force;
-} 
-
-task PackageZip -depends PackageModule -description "assembles zip archive of module distribution" -action {
-	$mp = get-modulePackageDirectory | Get-Item;
-	$pp = get-packageDirectory;
-	$zp = get-zipPackageName ;
-	
-	Import-Module pscx;
-	
-	Write-Verbose "module package path: $mp";
-	Write-Verbose "package path: $pp";
-	Write-Verbose "zip package name: $zp";
-	
-	write-zip -path "$mp\SQLite" -outputpath $zp | Move-Item -Destination $pp -Force ;	
+task __CreateNuGetPackageDirectory -description $private {
+    $p = get-nugetPackageDirectory;
+    $p  | create-packageDirectory;
+    @( 'tools','lib','content' ) | %{join-path $p -child $_ } | create-packageDirectory;
 }
 
-# install tasks
-
-task Uninstall -description "uninstalls the module from the local user module repository" {
-	$modulePath = $Env:PSModulePath -split ';' | select -First 1 | Join-Path -ChildPath 'SQLite';
-	if( Test-Path $modulePath )
+task __CreateLocalDataDirectory -description $private {
+	if( -not ( Test-Path $local ) )
 	{
-		Write-Verbose "uninstalling module from local module repository at $modulePath";
-		
-		$modulePath | ri -Recurse -force;
+		mkdir $local | Out-Null;
 	}
 }
 
-task Install -depends InstallModule -description "installs the module to the local machine";
+# primary targets
 
-task InstallModule -depends PackageModule -description "installs the module to the local user module repository" {
-	$packagePath = get-modulePackageDirectory;
-	$modulePath = $Env:PSModulePath -split ';' | select -First 1;
-	Write-Verbose "installing module from local module repository at $modulePath";
-	
-	ls $packagePath | Copy-Item -recurse -Destination $modulePath -Force;	
+task Build -depends __VerifyConfiguration -description "builds any outdated dependencies from source" {
+	exec { 
+		msbuild $slnFile /p:Configuration=$config /p:KeyContainerName=$keyContainer /t:Build 
+	}
 }
+
+task Clean -depends __VerifyConfiguration,CleanNuGet -description "deletes all temporary build artifacts" {
+	exec { 
+		msbuild $slnFile /p:Configuration=$config /t:Clean 
+	}
+}
+
+task Rebuild -depends Clean,Build -description "runs a clean build";
+
+task Package -depends Build -description "assembles distributions in the source hive" {
+
+}
+
+# clean tasks
+
+task CleanNuGet -depends __CreateNuGetPackageDirectory -description "clears the nuget package staging area" {
+    get-nugetPackageDirectory | 
+        ls | 
+        ?{ $_.psiscontainer } | 
+        ls | 
+        remove-item -recurse -force;
+}
+
+
+
 
